24a4d4c64b5d032047220b6cd2f1d5e312f8c46a
Benjamin Curtis <ben@bencurtis.com>
2018-02-20 09:49:01 -0800

    Release 0.5.4

--- honeybadger.js
+++ honeybadger.js
@@ -1,5 +1,5 @@
 /*
-  honeybadger.js v0.5.3
+  honeybadger.js v0.5.4
   A JavaScript Notifier for Honeybadger
   https://github.com/honeybadger-io/honeybadger-js
   https://www.honeybadger.io/
@@ -49,7 +49,7 @@
     root.Honeybadger = factory();
   }
 }(this, function () {
-  var VERSION = '0.5.3',
+  var VERSION = '0.5.4',
       NOTIFIER = {
         name: 'honeybadger.js',
         url: 'https://github.com/honeybadger-io/honeybadger-js',
