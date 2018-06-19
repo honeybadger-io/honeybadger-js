e43aa1db38aaf958f94b7d3b09e509d4841dc498
Joshua Wood <josh@joshuawood.net>
2018-06-19 11:18:42 -0700

    Fix continuing issues with this being undefined/undefined context (#96)
    * Rely on window explicitly.
    
    See #88, #89, #92. This also reverts the behavior in #92.
    
    We do this everywhere else, so we might as well do it here too. This
    should solve the issue of undefined contexts when logging by not relying
    on `this` being `window` in the factory builder.
    
    I like this approach better because if `root` is always window anyway,
    it's cleaner to call it that in the factory. `root` in the module
    definition is part of the [UMD pattern](https://github.com/umdjs/umd),
    and I'd like to keep it isolated.
    
    * Update UMD to explicitly detect browser global in strict mode.
    
    See https://github.com/umdjs/umd/commit/47e725735b854dfd93c1ca6fcebf1e018bd52606
    and https://github.com/umdjs/umd/commit/1deb860078252f31ced62fa8e7694f8bbfa6d889
    
    > In strict mode, `this` is no longer sufficient to detect the global
    > object. This change adapts the detection of the global object for the
    > case of exporting to the browser global object.
    
    Related to #88, #89, #92, #94

--- honeybadger.js
+++ honeybadger.js
@@ -28,7 +28,7 @@
   // Build the singleton factory. The factory can be accessed through
   // singleton.factory() to instantiate a new instance.
   var factory = function(){
-    var f = builder(root);
+    var f = builder();
     var singleton = f(scriptConfig);
     singleton.factory = f;
     return singleton;
@@ -48,7 +48,7 @@
     // Browser globals (root is window).
     root.Honeybadger = factory();
   }
-}(this, function (root) {
+}(typeof self !== 'undefined' ? self : this, function () {
   var VERSION = '0.5.4',
       NOTIFIER = {
         name: 'honeybadger.js',
@@ -178,7 +178,8 @@
     }
 
     function log() {
-      if (root.console) {
+      var console = window.console;
+      if (console) {
         var args = Array.prototype.slice.call(arguments);
         args.unshift('[Honeybadger]');
         console.log.apply(console, args);
@@ -187,7 +188,7 @@
 
     function debug() {
       if (config('debug')) {
-        return log.apply(root, arguments);
+        return log.apply(this, arguments);
       }
     }
 
@@ -238,7 +239,7 @@
       // Use XHR when available.
       try {
         // Inspired by https://gist.github.com/Xeoncross/7663273
-        var x = new(root.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+        var x = new(window.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
         x.open('GET', url, config('async', true));
         x.send();
         incrementErrorsCount();
