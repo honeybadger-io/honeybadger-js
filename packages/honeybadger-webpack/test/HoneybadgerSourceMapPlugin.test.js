ab5bc0dee1540eafc74e9cb1e437fefdaf2fdbf0
Oliver Benns <oliverbenns@gmail.com>
2018-07-03 14:43:23 +0200

    Make Webpack v4 default in tests

--- packages/honeybadger-webpack/test/HoneybadgerSourceMapPlugin.test.js
+++ packages/honeybadger-webpack/test/HoneybadgerSourceMapPlugin.test.js
@@ -5,7 +5,11 @@ import HoneybadgerSourceMapPlugin from '../src/HoneybadgerSourceMapPlugin';
 describe('HoneybadgerSourceMapPlugin', function() {
   beforeEach(function() {
     this.compiler = {
-      plugin: createSpy()
+      hooks: {
+        afterEmit: {
+          tapAsync: createSpy(),
+        },
+      },
     };
 
     this.options = {
@@ -41,28 +45,24 @@ describe('HoneybadgerSourceMapPlugin', function() {
   });
 
   describe('apply', function() {
-    it('should hook into "after-emit"', function() {
-      expect(this.compiler.plugin.calls.length).toBe(1);
-      expect(this.compiler.plugin.calls[0].arguments).toEqual([
-        'after-emit',
-        this.plugin.afterEmit.bind(this.plugin)
-      ]);
-    });
-
     it('should tap into "afterEmit" hook', function() {
-      this.compiler.hooks = {
-        afterEmit: {
-          tapAsync: createSpy(),
-        }
-      }
-
-      this.plugin.apply(this.compiler);
-
       const { afterEmit } = this.compiler.hooks
 
       expect(afterEmit.tapAsync.calls.length).toBe(1);
       expect(afterEmit.tapAsync.calls[0].arguments).toEqual([
         'HoneybadgerSourceMapPlugin',
+        this.plugin.afterEmit.bind(this.plugin)
+      ]);
+    });
+
+    it('should hook into "after-emit"', function() {
+      this.compiler.hooks = null
+      this.compiler.plugin = createSpy()
+      this.plugin.apply(this.compiler);
+
+      expect(this.compiler.plugin.calls.length).toBe(1);
+      expect(this.compiler.plugin.calls[0].arguments).toEqual([
+        'after-emit',
         this.plugin.afterEmit.bind(this.plugin)
       ]);
     });
