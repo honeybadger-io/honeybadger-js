7c1431e1f312554354fc7f6ebffa54575105ac4e
Joshua Wood <josh@joshuawood.net>
2019-02-08 14:15:26 -0800

    test latest versions on Windows 10

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,10 @@ module.exports = function(grunt) {
     platform: 'Windows 10'
   }, {
     browserName: 'googlechrome',
-    platform: 'OS X 10.11'
-  }, {
-    browserName: 'googlechrome',
-    platform: 'linux'
+    platform: 'Windows 10'
   }, {
     browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '14.14393'
+    platform: 'Windows 10'
   }, {
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
@@ -47,7 +43,7 @@ module.exports = function(grunt) {
     'saucelabs-jasmine': {
       all: {
         options: {
-          urls: ['http://127.0.0.1:9999/spec/runner.html'],
+          urls: ['http://localhost:9999/spec/runner.html'],
           build: process.env.TRAVIS_JOB_ID,
           browsers: browsers,
           testname: 'honeybadger.js specs',
