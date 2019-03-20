dc215f1ec03a2167fc020a9b72351cb82452a7ad
Joshua Wood <josh@joshuawood.net>
2019-03-19 17:58:29 -0700

    Support beta tags on CDN

--- scripts/release-cdn.sh
+++ scripts/release-cdn.sh
@@ -7,13 +7,19 @@ if [[ -z "${HONEYBADGER_JS_S3_BUCKET}" ]]; then
   exit 1
 fi
 
-VERSION=$(cat package.json | jq -r '.version | capture("^(?<match>[0-9]+.[0-9]+).[0-9]+$") | .match')
+VERSION_INFO=$(cat package.json | jq '.version | capture("^(?<minor>[0-9]+.[0-9]+).[0-9]+((?<tag>-beta).[0-9]+)?$")')
+VERSION=$(echo $VERSION_INFO | jq -r '.minor')
+TAG=$(echo $VERSION_INFO | jq -r '.tag')
+
 if [[ -z "${VERSION}" ]]; then
   echo "Not a production version; skipping CDN upload."
   exit
 fi
 
 PREFIX=v$VERSION
+if [ "$TAG" != "null" ] && [ ! -z "$TAG" ]; then
+  PREFIX=$PREFIX$TAG
+fi
 
 aws s3 sync dist/ s3://$HONEYBADGER_JS_S3_BUCKET/$PREFIX \
   --acl 'public-read' \
