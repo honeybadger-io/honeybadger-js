1a24829b30185e91b6348c07ede46920ed68ba0e
Joshua Wood <josh@joshuawood.net>
2019-03-20 14:00:31 -0700

    Add pre/post version scripts

---
+++ scripts/update-versions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+VERSION=$(cat package.json | jq -r '.version')
+DATE=$(date +"%Y-%m-%d")
+
+# Update bower.json
+tmp=$(mktemp)
+cat bower.json | jq ".version |= \"$VERSION\"" > "$tmp" && mv "$tmp" bower.json
+
+# Update CHANGELOG.md
+sed -i "s/## \[Unreleased\]/## \[Unreleased\]\n\n## \[$VERSION\] - $DATE/" CHANGELOG.md
