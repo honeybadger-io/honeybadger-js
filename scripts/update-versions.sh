542cefb1728d0a11cc150406e3a86e3baeda54e7
Joshua Wood <josh@joshuawood.net>
2019-03-20 14:38:07 -0700

    Add version check

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -5,6 +5,11 @@ set -e
 VERSION=$(cat package.json | jq -r '.version')
 DATE=$(date +"%Y-%m-%d")
 
+if grep -q "$VERSION" "CHANGELOG.md"; then
+  echo "Error: $VERSION already exists in CHANGELOG.md"
+  exit 1
+fi
+
 # Update bower.json
 tmp=$(mktemp)
 cat bower.json | jq ".version |= \"$VERSION\"" > "$tmp" && mv "$tmp" bower.json
