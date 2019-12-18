13063453dcad5e5642fb6ba251c4e18662d5500e
Jennie Chamberlin <jennie@trunkclub.com>
2019-12-18 15:40:05 -0500

    adds check to prevent an error when a null error is passed with arguments and ignore patterns to notify fixes #266

--- src/builder.js
+++ src/builder.js
@@ -47,6 +47,8 @@ export default function builder() {
   function isIgnored(err, patterns) {
     var msg = err.message;
 
+    if (!msg)  { return false; }
+
     for (var p in patterns) {
       if (msg.match(patterns[p])) { return true; }
     }
