2ca453ca7e83a63c39c54ebeae9c54a5e978426a
Victor <bury.victor@gmail.com>
2020-03-14 00:53:10 +0100

    feat(after-notify): hook to execute handlers after the request was sent (#314)
    * feat(after-notify): hook to execute handlers after the request was sent
    
    * feat(after-notify): return the request response and error
    
    * feat(tests): add afterNotify success and error flow
    
    Co-authored-by: Victor Bury <victor@c02wx0kag8wn.lan>
    Co-authored-by: Victor Bury <victor@c02wx0kag8wn.local>

--- src/builder.js
+++ src/builder.js
@@ -116,11 +116,11 @@ export default function builder() {
     return {stack: stack.join('\n'), generator: 'walk'};
   }
 
-  function checkHandlers(handlers, err) {
+  function checkHandlers(handlers, err, response) {
     var handler, i, len;
     for (i = 0, len = handlers.length; i < len; i++) {
       handler = handlers[i];
-      if (handler(err) === false) {
+      if (handler(err, response) === false) {
         return true;
       }
     }
@@ -149,6 +149,7 @@ export default function builder() {
     var self = {
       context: {},
       beforeNotifyHandlers: [],
+      afterNotifyHandlers: [],
       breadcrumbs: [],
       errorsSent: 0,
     };
@@ -207,7 +208,17 @@ export default function builder() {
         x.setRequestHeader('Accept', 'text/json, application/json');
 
         x.send(JSON.stringify(sanitize(payload, config('max_depth', 8))));
+        x.onload = function() {
+          if (x.status !== 201) {
+            checkHandlers(self.afterNotifyHandlers, JSON.parse(x.response));
+            debug(`Unable to send error report: ${x.status}: ${x.statusText}`);
+            return;
+          }
+          checkHandlers(self.afterNotifyHandlers, undefined, JSON.parse(x.response));
+          debug('Error report sent');
+        };
       } catch(err) {
+        checkHandlers(self.afterNotifyHandlers, err);
         log('Unable to send error report: error while initializing request', err, payload);
       }
     }
@@ -464,6 +475,11 @@ export default function builder() {
 
     self.beforeNotify = function(handler) {
       self.beforeNotifyHandlers.push(handler);
+      return self;
+    };
+
+    self.afterNotify = function(handler) {
+      self.afterNotifyHandlers.push(handler);
       return self;
     };
 
