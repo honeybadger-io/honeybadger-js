d2a5267d3ea770b75cbf3652eb3e488e7b7e6407
Joshua Wood <josh@joshuawood.net>
2020-11-06 13:19:21 -0800

    Get next version from changelog

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,75 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const semver = require('semver')
+
+// eslint-disable-next-line no-undef
 module.exports = {
-  publishCommand: ({ defaultCommand, tag }) =>
-    `${defaultCommand} --access public --tag ${tag}`,
-};
+  updateChangelog: false,
+  formatCommitMessage: ({ version, _releaseType, _baseBranch }) => `Release v${version}`,
+  formatPullRequestTitle: ({ version, _releaseType }) => `Release v${version}`,
+  getNextVersion: ({ _revisionRange, _commitTitles, _commitBodies, currentVersion, dir }) => {
+    const changelog = new Changelog(`${dir}/CHANGELOG.md`)
+    return changelog.nextVersion(currentVersion)
+  },
+  versionUpdated: ({ _version, _releaseType, _dir, _exec }) => {
+    // Update CHANGELOG.md heading for latest release
+    true
+  }
+}
+
+class Changelog {
+  releaseType = "patch"
+  releaseTag = "latest"
+
+  constructor(path) {
+    const data = fs.readFileSync(path, 'utf8')
+    const lines = data.split(/\r?\n/)
+    const headings = []
+    let unreleased = false
+    lines.every((line) => {
+      if (line.startsWith("## [Unreleased]")) {
+        unreleased = true
+        const tagMatch = line.match(/## \[Unreleased\]\[(.*)\]/)
+        if (tagMatch) {
+          this.releaseTag = tagMatch[1].trim()
+        }
+      } else if (line.startsWith("## ")) {
+        return false
+      }
+
+      if (unreleased) {
+        if (line.startsWith("### ")) {
+          headings.push(line.match(/### (.*)/)[1].trim())
+        }
+      }
+
+      return true
+    });
+
+    if (headings.includes("Changed")) {
+      this.releaseType = "major"
+    } else if (headings.includes("Added")) {
+      this.releaseType = "minor"
+    } else if (headings.includes("Fixed")) {
+      this.releaseType = "patch"
+    }
+  }
+
+  nextVersion(version) {
+    const parsedVersion = semver.parse(version)
+
+    if (this.releaseTag !== "latest") {
+      if (parsedVersion.prerelease.length) {
+        parsedVersion.inc("prerelease", this.releaseTag)
+      } else {
+        parsedVersion.inc(this.releaseType)
+        parsedVersion.prerelease = [ this.releaseTag, 0 ]
+        parsedVersion.format()
+      }
+    } else {
+      parsedVersion.inc(this.releaseType)
+    }
+
+    return parsedVersion.version
+  }
+}
