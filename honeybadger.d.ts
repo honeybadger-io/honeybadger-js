89bdd08354cf5989aae2adebff769b858090f503
shalvah <diakon.ng@gmail.com>
2021-07-16 17:32:08 +0100

    Simplify exported type declarations

--- honeybadger.d.ts
+++ honeybadger.d.ts
@@ -2,31 +2,6 @@
 // Project: https://github.com/honeybadger-io/honeybadger-js
 import Server from './dist/server/types/server'
 import Browser from './dist/browser/types/browser'
-import {
-    Logger as BaseLogger,
-    Config as BaseConfig,
-    BeforeNotifyHandler as BaseBeforeNotifyHandler,
-    AfterNotifyHandler as BaseAfterNotifyHandler,
-    Notice as BaseNotice,
-    BacktraceFrame as BaseBacktraceFrame,
-    BreadcrumbRecord as BaseBreadcrumbRecord,
-} from './dist/server/types/core/types'
 
-declare namespace Honeybadger {
-    type Logger = BaseLogger
-
-    type Config = BaseConfig
-
-    type BeforeNotifyHandler = BaseBeforeNotifyHandler
-
-    type AfterNotifyHandler = BaseAfterNotifyHandler
-
-    type Notice = BaseNotice
-
-    type BacktraceFrame = BaseBacktraceFrame
-
-    type BreadcrumbRecord = BaseBreadcrumbRecord
-}
-
-declare const singleton: typeof Server & typeof Browser
-export = singleton
+declare const Honeybadger: typeof Server & typeof Browser
+export = Honeybadger
