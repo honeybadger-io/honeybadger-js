73aaa89b3385c7980c6e742f774098e6e7c007b1
Pangratios Cosma <pagcosma@gmail.com>
2022-09-18 16:39:54 +0300

    chore: add all package versions to event

---
+++ .github/actions/get-package-versions/README.md
@@ -0,0 +1,33 @@
+# Get Monorepo Package Versions Github Action
+
+This action reads package versions from the monorepo and stores them as an array to a variable.
+
+## Outputs
+
+### `packages`
+
+A stringified array in the structure of:
+
+```json
+[
+  {
+    "name": "@honeybadger-io/core",
+    "version": "4.2.1"
+  },
+  {
+    "name": "@honeybadger-io/js",
+    "version": "4.3"
+  }
+]
+```
+
+## Example usage
+
+uses: .github/actions/get-package-versions
+
+## Developing
+
+1. Modify `index.js`
+2. Install `npm i -g @vercel/ncc` to compile action to a single file 
+3. Run `npm run build`
+4. Make sure you commit `dist/index.js` for your changes to take effect
---
+++ .github/actions/get-package-versions/index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core')
+const fs = require('fs')
+const path = require('path')
+
+(async function () {
+  const ROOT_PACKAGES_FOLDER = 'packages'
+  core.info('Reading package versions from monorepo')
+  const result = []
+  const packages = await fs.promises.readdir(ROOT_PACKAGES_FOLDER)
+  for (let i = 0; i < packages.length; i++) {
+    const p = packages[i]
+    const packageJsonPath = path.join(ROOT_PACKAGES_FOLDER, p, 'package.json')
+    try {
+      const packageJson = JSON.parse(await fs.promises.readFile(packageJsonPath, 'utf8'))
+      core.info(`${packageJson.name}: ${packageJson.version}`)
+      result.push({
+        name: packageJson.name,
+        version: packageJson.version
+      })
+    }
+    catch (error) {
+      core.error(`There was an error reading package[${p}]: ${error.message}`)
+    }
+  }
+  core.setOutput('packages', JSON.stringify(result))
+  core.info('Done')
+})()
