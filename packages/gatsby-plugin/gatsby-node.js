a0f958e710e812cfae9e681f1a6d57e4d5affc0f
Pangratios Cosma <pagcosma@gmail.com>
2022-10-01 18:59:23 +0300

    chore: npm lint

--- packages/gatsby-plugin/gatsby-node.js
+++ packages/gatsby-plugin/gatsby-node.js
@@ -1,13 +1,13 @@
 const HoneybadgerSourceMapPlugin = require('@honeybadger-io/webpack')
 
-exports.onCreateWebpackConfig = ({ actions}, { apiKey, revision, assetsUrl }) => {
-   actions.setWebpackConfig({
-     plugins: [
+exports.onCreateWebpackConfig = ({ actions }, { apiKey, revision, assetsUrl }) => {
+  actions.setWebpackConfig({
+    plugins: [
       new HoneybadgerSourceMapPlugin({
         apiKey,
         revision,
         assetsUrl
       })
-     ]
-   })
- }
+    ]
+  })
+}
