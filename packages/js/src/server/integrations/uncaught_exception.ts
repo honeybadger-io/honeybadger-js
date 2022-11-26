ffff05101abf8251ff4cf1289a11a5fdedca060b
Pangratios Cosma <pagcosma@gmail.com>
2022-11-27 00:26:29 +0200

    fix: exit process on unhandledRejection (#950)

--- packages/js/src/server/integrations/uncaught_exception.ts
+++ packages/js/src/server/integrations/uncaught_exception.ts
@@ -3,7 +3,8 @@ import { fatallyLogAndExit } from '../util'
 import Client from '../../server'
 import { removeAwsDefaultUncaughtExceptionListener } from '../aws_lambda'
 
-let count = 0
+let isReporting = false
+let handlerAlreadyCalled = false
 
 function removeAwsLambdaListener() {
   const isLambda = !!process.env.LAMBDA_TASK_ROOT
@@ -14,28 +15,58 @@ function removeAwsLambdaListener() {
   removeAwsDefaultUncaughtExceptionListener()
 }
 
+/**
+ * If there are no other uncaughtException listeners,
+ * we want to report the exception to Honeybadger and
+ * mimic the default behavior of NodeJs,
+ * which is to exit the process with code 1
+ */
+function hasOtherUncaughtExceptionListeners() {
+  return process.listeners('uncaughtException').length > 1
+}
+
 export default function (): Types.Plugin {
   return {
     load: (client: typeof Client) => {
-      if (!client.config.enableUncaught) { return }
+      if (!client.config.enableUncaught) {
+        return
+      }
 
       removeAwsLambdaListener()
 
-      process.on('uncaughtException', function honeybadgerUncaughtExceptionListener(uncaughtError) {
-        // Prevent recursive errors
-        if (count > 1) { fatallyLogAndExit(uncaughtError) }
 
-        if (client.config.enableUncaught) {
-          client.notify(uncaughtError, {
-            afterNotify: (_err, _notice) => {
-              count += 1
-              client.config.afterUncaught(uncaughtError)
-            }
-          })
-        } else {
-          count += 1
+      process.on('uncaughtException', function honeybadgerUncaughtExceptionListener(uncaughtError) {
+        if (!client.config.enableUncaught) {
           client.config.afterUncaught(uncaughtError)
+          if (!hasOtherUncaughtExceptionListeners()) {
+            fatallyLogAndExit(uncaughtError)
+          }
+          return
         }
+
+        // report only the first error - prevent reporting recursive errors
+        if (handlerAlreadyCalled) {
+          if (!hasOtherUncaughtExceptionListeners()) {
+            fatallyLogAndExit(uncaughtError)
+          }
+          return
+        }
+
+        if (isReporting) {
+          return
+        }
+
+        isReporting = true
+        client.notify(uncaughtError, {
+          afterNotify: (_err, _notice) => {
+            isReporting = false
+            handlerAlreadyCalled = true
+            client.config.afterUncaught(uncaughtError)
+            if (!hasOtherUncaughtExceptionListeners()) {
+              fatallyLogAndExit(uncaughtError)
+            }
+          }
+        })
       })
     }
   }
