846e23998f3f5d75cd5d91d82ca62a87886d47ee
Konnor Rogers <konnor5456@gmail.com>
2023-01-05 17:59:55 -0500

    ff 80?

--- packages/js/test/integration/browsers.js
+++ packages/js/test/integration/browsers.js
@@ -16,7 +16,7 @@ module.exports = {
   bs_firefox: {
     base: 'BrowserStack',
     browser: 'Firefox',
-    browser_version: '74.0',
+    browser_version: '80.0',
     os: 'Windows',
     os_version: '10'
   },
