be648617d3b5277cbc28c120b90b5a2e44bc7311
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-02-24 12:40:31 -0500

    New example projects

---
+++ packages/react-native/examples/expo-app/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+import Honeybadger from '@honeybadger-io/react-native';
+
+export default function App() {
+  const [apiKey, setApiKey] = useState("");
+  
+  function onConfigureButtonPress() {
+    console.log('Configuring HB with API key:', apiKey)
+    Honeybadger.configure(apiKey)
+  }
+  function onErrButtonPress() {
+    throw ( new Error('This is a test error from the react-native example project!') );
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+          placeholder="enter your API key"
+          value={apiKey}
+          onChangeText={(text) => setApiKey(text)}
+      />
+      <Button onPress={ onConfigureButtonPress } title="Configure HB" />
+      <Button onPress={ onErrButtonPress } title="Throw an error!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ packages/react-native/examples/react-native-cli/App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import { SafeAreaView, Button, TextInput } from 'react-native';
+import Honeybadger from '@honeybadger-io/react-native';
+
+export default function App() {
+  const [apiKey, setApiKey] = useState("");
+  
+  function onConfigureButtonPress() {
+    console.log('Configuring HB with API key:', apiKey)
+    Honeybadger.configure(apiKey)
+  }
+  function onErrButtonPress() {
+    throw ( new Error('This is a test error from the react-native example project!') );
+  }
+
+  return (
+    <SafeAreaView>
+      <TextInput
+          placeholder="enter your API key"
+          value={apiKey}
+          onChangeText={(text) => setApiKey(text)}
+      />
+      <Button onPress={ onConfigureButtonPress } title="Configure HB" />
+      <Button onPress={ onErrButtonPress } title="Throw an error!" />
+    </SafeAreaView>
+  );
+
+}
