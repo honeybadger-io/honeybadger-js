3721023877e8bc455f5f444c4b0ec2f7e59bc02d
Bethany Berkowitz <berkowitz.bethany@gmail.com>
2023-03-10 10:36:34 -0500

    chore(react-native): updates to example projects  (#1034)

--- packages/react-native/examples/expo-app/App.js
+++ packages/react-native/examples/expo-app/App.js
@@ -4,13 +4,19 @@ import Honeybadger from '@honeybadger-io/react-native';
 
 export default function App() {
   const [apiKey, setApiKey] = useState('');
+  const [revision, setRevision] = useState('testRevisionExpo123');
   
   function onConfigureButtonPress() {
     console.log('Configuring HB with API key:', apiKey)
-    Honeybadger.configure(apiKey)
+    Honeybadger.configure(
+      apiKey, 
+      true,
+      revision,
+    )
+    Honeybadger.setLogLevel('debug')
   }
   function onErrButtonPress() {
-    throw ( new Error('This is a test error from the react-native example project!') );
+    throw ( new Error('This is a test error from the react-native expo-app example project!') );
   }
 
   return (
@@ -19,6 +25,11 @@ export default function App() {
         placeholder="enter your API key"
         value={apiKey}
         onChangeText={(text) => setApiKey(text)}
+      />
+      <TextInput
+        placeholder="enter your revision"
+        value={revision}
+        onChangeText={(text) => setRevision(text)}
       />
       <Button onPress={ onConfigureButtonPress } title="Configure HB" />
       <Button onPress={ onErrButtonPress } title="Throw an error!" />
--- packages/react-native/examples/react-native-cli/App.tsx
+++ packages/react-native/examples/react-native-cli/App.tsx
@@ -9,17 +9,27 @@ import React, { useState } from 'react';
 import { SafeAreaView, Button, TextInput } from 'react-native';
 import Honeybadger from '@honeybadger-io/react-native';
 
+Honeybadger.setLogLevel('debug')
+
 export default function App() {
   const [apiKey, setApiKey] = useState('');
+  const [revision, setRevision] = useState('testRevisionCli123');
 
   function onConfigureButtonPress() {
     console.log('Configuring HB with API key:', apiKey);
-    Honeybadger.configure(apiKey);
+    Honeybadger.configure(
+      apiKey, 
+      true,
+      revision,
+    )
   }
   function onErrButtonPress() {
     throw new Error(
-      'This is a test error from the react-native example project!',
+      'This is a test error from the react-native-cli example project!',
     );
+  }
+  function onNotifyButtonPress() {
+    Honeybadger.notify(new Error('This is a test notify() from the react-native-cli example project'), {})
   }
 
   return (
@@ -29,8 +39,14 @@ export default function App() {
         value={apiKey}
         onChangeText={text => setApiKey(text)}
       />
+      <TextInput
+        placeholder="enter your revision"
+        value={revision}
+        onChangeText={(text) => setRevision(text)}
+      />
       <Button onPress={onConfigureButtonPress} title="Configure HB" />
       <Button onPress={onErrButtonPress} title="Throw an error!" />
+      <Button onPress={onNotifyButtonPress} title="Honeybader.notify()" />
     </SafeAreaView>
   );
 }
