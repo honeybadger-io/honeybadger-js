c1233061fffc3374fffcb58a94af60da52b48a38
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2023-04-14 13:58:30 +0300

    chore(nextjs): _error.js documentation

--- packages/nextjs/templates/_error.js
+++ packages/nextjs/templates/_error.js
@@ -2,6 +2,13 @@
 import { notifyFromNextErrorComponent } from '@honeybadger-io/nextjs';
 import NextErrorComponent from 'next/error';
 
+/**
+ * This component is called when:
+ *  - on the server, when data fetching methods throw or reject
+ *  - on the client, when getInitialProps throws or rejects
+ *  - on the client, when a React lifecycle method (render, componentDidMount, etc) throws or rejects
+ *      and was caught by the built-in Next.js error boundary
+ */
 const CustomErrorComponent = props => {
   return <NextErrorComponent statusCode={props.statusCode} />;
 };
