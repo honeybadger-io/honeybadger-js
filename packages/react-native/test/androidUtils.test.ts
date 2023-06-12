5b4256c85fec7199bd81680551ac330575e74bdc
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-06-12 17:19:22 -0400

    Use real err data for android test

--- packages/react-native/test/androidUtils.test.ts
+++ packages/react-native/test/androidUtils.test.ts
@@ -1,5 +1,33 @@
 import { backtraceFromAndroidException } from '../src/androidUtils'
 
+// Pulled this example data from a native error thrown in 
+// examples/react-native-cli running in dev mode (npm run android)
+// Shortened the callstack for simplicity
+const data = {
+  stackTrace: [
+    {
+      line: 30,
+      class: "com.awesomeproject.ThrowErrModule$1",
+      method: "run",
+      file: "ThrowErrModule.java"
+    },
+    {
+      line: 938,
+      class: "android.os.Handler",
+      method: "handleCallback",
+      file: "Handler.java"
+    },
+    {
+      line: 99,
+      class: "android.os.Handler",
+      method: "dispatchMessage",
+      file: "Handler.java"
+    },
+  ],
+  message: "Test Delayed Exception",
+  type: "Exception"
+}
+
 describe('androidUtils', () => {
   describe('backtraceFromAndroidException', () => {
     it('returns an empty array if stackTrace is missing', () => {
@@ -8,20 +36,7 @@ describe('androidUtils', () => {
     })
 
     it('returns an array of frames', () => {
-      const stackTrace = [
-        {
-          class: 'com.awesomeproject.ThrowErrModule$1',
-          file: 'ThrowErrModule.java',
-          line: 30,
-          method: 'run',
-        },
-        {
-          file: 'Handler.java',
-          line: 938,
-          method: 'handleCallback',
-        },
-      ]
-      const result = backtraceFromAndroidException({ type: 'error', stackTrace})
+      const result = backtraceFromAndroidException(data)
       expect(result).toStrictEqual([
         {
           method: 'com.awesomeproject.ThrowErrModule$1.run',
@@ -29,10 +44,15 @@ describe('androidUtils', () => {
           number: 30,
         }, 
         {
-          method: 'handleCallback',
+          method: 'android.os.Handler.handleCallback',
           file: 'Handler.java',
           number: 938,
         },
+        {
+          method: "android.os.Handler.dispatchMessage",
+          file: "Handler.java", 
+          number: 99,
+        }       
       ])
     })
   })
