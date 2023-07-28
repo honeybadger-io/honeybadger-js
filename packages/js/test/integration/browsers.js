cceeeb0159752e53a6dd2d717e522077aea44060
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-07-28 13:32:27 -0400

    Set min browsers to test against

--- packages/js/test/integration/browsers.js
+++ packages/js/test/integration/browsers.js
@@ -1,29 +1,58 @@
 module.exports = {
-  bs_safari: {
+  bs_safari_min: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: '10.1',
+    os: 'OS X',
+    os_version: 'Sierra'
+  },
+  bs_safari_latest: {
     base: 'BrowserStack',
     browser: 'Safari',
     browser_version: 'latest',
     os: 'OS X',
-    os_version: 'Mojave'
+    os_version: 'Ventura'
   },
-  bs_chrome: {
+  bs_chrome_min: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    browser_version: '44',
+    os: 'Windows',
+    os_version: '8'
+  },
+  bs_chrome_latest: {
     base: 'BrowserStack',
     browser: 'Chrome',
     browser_version: 'latest',
     os: 'Windows',
     os_version: '10'
   },
-  bs_firefox: {
+  bs_firefox_min: {
     base: 'BrowserStack',
     browser: 'Firefox',
-    browser_version: '108.0',
+    browser_version: '40',
+    os: 'Windows',
+    os_version: '8'
+  },
+  bs_firefox_latest: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: 'latest',
     os: 'Windows',
     os_version: '10'
   },
-  bs_edge: {
+  bs_edge_min: {
     base: 'BrowserStack',
     browser: 'Edge',
+    browser_version: '15',
     os: 'Windows',
     os_version: '10'
+  },
+  bs_edge_latest: {
+    base: 'BrowserStack',
+    browser: 'Edge',
+    browser_version: 'latest',
+    os: 'Windows',
+    os_version: '11'
   }
 }
