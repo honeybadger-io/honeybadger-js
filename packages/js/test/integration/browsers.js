698cd818f4a150e47be0ad5fa7bb40202a28052f
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-08-02 09:47:53 -0400

    Check if error is only on firefox

--- packages/js/test/integration/browsers.js
+++ packages/js/test/integration/browsers.js
@@ -1,58 +1,58 @@
 module.exports = {
-  // bs_safari_min: {
-  //   base: 'BrowserStack',
-  //   browser: 'Safari',
-  //   browser_version: '10.1',
-  //   os: 'OS X',
-  //   os_version: 'Sierra'
-  // },
-  // bs_safari_latest: {
-  //   base: 'BrowserStack',
-  //   browser: 'Safari',
-  //   browser_version: 'latest',
-  //   os: 'OS X',
-  //   os_version: 'Ventura'
-  // },
-  // bs_chrome_min: {
-  //   base: 'BrowserStack',
-  //   browser: 'Chrome',
-  //   browser_version: '49',
-  //   os: 'Windows',
-  //   os_version: '10'
-  // },
-  // bs_chrome_latest: {
-  //   base: 'BrowserStack',
-  //   browser: 'Chrome',
-  //   browser_version: 'latest',
-  //   os: 'Windows',
-  //   os_version: '11'
-  // },
-  bs_firefox_min: {
+  bs_safari_min: {
     base: 'BrowserStack',
-    browser: 'Firefox',
-    browser_version: '65',
+    browser: 'Safari',
+    browser_version: '10.1',
+    os: 'OS X',
+    os_version: 'Sierra'
+  },
+  bs_safari_latest: {
+    base: 'BrowserStack',
+    browser: 'Safari',
+    browser_version: 'latest',
+    os: 'OS X',
+    os_version: 'Ventura'
+  },
+  bs_chrome_min: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    browser_version: '49',
     os: 'Windows',
     os_version: '10'
   },
-  // bs_firefox_latest: {
+  bs_chrome_latest: {
+    base: 'BrowserStack',
+    browser: 'Chrome',
+    browser_version: 'latest',
+    os: 'Windows',
+    os_version: '11'
+  },
+  // bs_firefox_min: {
   //   base: 'BrowserStack',
   //   browser: 'Firefox',
-  //   browser_version: 'latest',
+  //   browser_version: '65',
   //   os: 'Windows',
   //   os_version: '10'
   // },
-  // bs_edge_min: {
-  //   base: 'BrowserStack',
-  //   browser: 'Edge',
-  //   browser_version: '15',
-  //   os: 'Windows',
-  //   os_version: '10'
-  // },
-  // bs_edge_latest: {
-  //   base: 'BrowserStack',
-  //   browser: 'Edge',
-  //   browser_version: 'latest',
-  //   os: 'Windows',
-  //   os_version: '11'
-  // }
+  bs_firefox_latest: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: 'latest',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_edge_min: {
+    base: 'BrowserStack',
+    browser: 'Edge',
+    browser_version: '15',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_edge_latest: {
+    base: 'BrowserStack',
+    browser: 'Edge',
+    browser_version: 'latest',
+    os: 'Windows',
+    os_version: '11'
+  }
 }
