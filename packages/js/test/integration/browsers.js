518555badae94f3bd8e1e81008f1e063454b90e7
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-08-02 11:24:42 -0400

    Find min firefox version

--- packages/js/test/integration/browsers.js
+++ packages/js/test/integration/browsers.js
@@ -1,12 +1,11 @@
 module.exports = {
-  bs_safari_min: {
-    base: 'BrowserStack',
-    browser: 'Safari',
-    browser_version: '12.1',
-    os: 'OS X',
-    os_version: 'Mojave'
-  },
-  
+  // bs_safari_min: {
+  //   base: 'BrowserStack',
+  //   browser: 'Safari',
+  //   browser_version: '12.1',
+  //   os: 'OS X',
+  //   os_version: 'Mojave'
+  // },
   // bs_safari_latest: {
   //   base: 'BrowserStack',
   //   browser: 'Safari',
@@ -28,13 +27,41 @@ module.exports = {
   //   os: 'Windows',
   //   os_version: '11'
   // },
-  // bs_firefox_min: {
-  //   base: 'BrowserStack',
-  //   browser: 'Firefox',
-  //   browser_version: '65',
-  //   os: 'Windows',
-  //   os_version: '10'
-  // },
+  bs_firefox_65: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '65',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_firefox_75: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '75',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_firefox_85: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '85',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_firefox_95: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '95',
+    os: 'Windows',
+    os_version: '10'
+  },
+  bs_firefox_105: {
+    base: 'BrowserStack',
+    browser: 'Firefox',
+    browser_version: '105',
+    os: 'Windows',
+    os_version: '10'
+  },
   // bs_firefox_latest: {
   //   base: 'BrowserStack',
   //   browser: 'Firefox',
