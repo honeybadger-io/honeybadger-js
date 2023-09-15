e665ecc5297c3059033cf3d99077fbb4b37fe5bf
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2023-09-15 22:52:41 +0300

    chore: enable all browsers

--- packages/js/test/e2e/browsers.ts
+++ packages/js/test/e2e/browsers.ts
@@ -74,42 +74,40 @@ const browserStackBrowsers = [
 ]
 
 const playwrightBrowsers = [
-  // {
-  //   name: 'chromium',
-  //   use: { ...devices['Desktop Chrome'] },
-  // },
+  {
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'] },
+  },
   {
     name: 'firefox',
     use: { ...devices['Desktop Firefox'] },
   },
-  // {
-  //   name: 'webkit',
-  //   use: { ...devices['Desktop Safari'] },
-  // },
-  //
-  // /* Test against mobile viewports. */
-  // {
-  //   name: 'Mobile Chrome',
-  //   use: { ...devices['Pixel 5'] },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   use: { ...devices['iPhone 12'] },
-  // },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] },
+  },
+
+  /* Test against mobile viewports. */
+  {
+    name: 'Mobile Chrome',
+    use: { ...devices['Pixel 5'] },
+  },
+  {
+    name: 'Mobile Safari',
+    use: { ...devices['iPhone 12'] },
+  },
 
   /* Test against branded browsers. */
-  // {
-  //   name: 'Microsoft Edge',
-  //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-  // },
-  // {
-  //   name: 'Google Chrome',
-  //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-  // },
+  {
+    name: 'Microsoft Edge',
+    use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  },
+  {
+    name: 'Google Chrome',
+    use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  },
 ]
 
-export const browsers = playwrightBrowsers
-
-// export const browsers = (!!process.env.CI || !!process.env.BROWSERSTACK_ACCESS_KEY)
-//   ? [...browserStackBrowsers, ...playwrightBrowsers]
-//   : playwrightBrowsers
+export const browsers = (!!process.env.CI || !!process.env.BROWSERSTACK_ACCESS_KEY)
+  ? [...browserStackBrowsers, ...playwrightBrowsers]
+  : playwrightBrowsers
