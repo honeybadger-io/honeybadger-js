2399bb303cd75189864179df58649bc12e50b3a0
Bethany Berkowitz <berkowitz.bethany@gmail.com>
2023-10-13 11:16:41 -0400

    Move index.html out of dist

--- packages/webpack/example/webpack.config.js
+++ packages/webpack/example/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'production',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'source-map',
