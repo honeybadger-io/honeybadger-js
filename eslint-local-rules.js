207ca8efb67085d564a8db6680e1bc7a8fa582be
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-01-03 12:02:31 +0200

    fix(react-native): remove wrong import
    Also creates a custom eslint rule that will prevent similar errors in the future
    
    Fixes: #1278

---
+++ eslint-local-rules.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const TEST_FILE_REGEX= /\.(spec|test)\.(js|ts|tsx)/
+// we can add more modules here
+const TEST_MODULES_REGEX= /jest/
+
+module.exports = {
+  'no-test-imports': {
+    meta: {
+      type: 'problem',
+      docs: {
+        description: 'Disallow importing test files in source code',
+      },
+      fixable: 'code',
+      schema: [] // no options
+    },
+    create: function(context) {
+      return {
+        ImportDeclaration: function(node) {
+          const physicalFilename = context.getPhysicalFilename().toLowerCase();
+
+          // stop if the filename is not in the src/ directory or is a test file
+          if (!physicalFilename.includes('src/') ||
+              physicalFilename.match(TEST_FILE_REGEX)) {
+            return;
+          }
+
+          // if we get here, we're in a source file, not a test file
+          const importSource = node.source.value.trim();
+          // do not allow importing test files or modules that include 'jest', such as 'jest-fetch-mock'
+          if (importSource.match(TEST_FILE_REGEX) ||
+              importSource.match(TEST_MODULES_REGEX)) {
+            context.report({
+              node: node,
+              message: 'Do not import test modules in source code',
+              fix: function(fixer) {
+                return fixer.remove(node);
+              }
+            });
+          }
+        }
+      };
+    }
+  }
+};
