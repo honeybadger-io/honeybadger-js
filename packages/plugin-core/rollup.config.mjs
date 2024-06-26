4d4a930fc48ee42e76e03efa6d84f313ee75b4a5
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2024-06-26 11:24:47 +0300

    chore: fix rollup for plugin-core

--- packages/plugin-core/rollup.config.mjs
+++ packages/plugin-core/rollup.config.mjs
@@ -6,21 +6,26 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      dir: 'dist', 
+      dir: 'dist',
       format: 'cjs',
       sourcemap: true,
     },
-    plugins: [ 
+    plugins: [
       typescript(),
-    ], 
+    ],
     external: [ 'node-fetch', 'form-data', 'fs', 'fetch-retry' ]
-  }, 
+  },
   {
     input: 'dist/dts/index.d.ts',
     output: { file: 'dist/index.d.ts' },
-    plugins: [ 
-      dts(), 
-      del({ hook: "buildEnd", targets: "./dist/dts" })
+    plugins: [
+      dts(),
+      // we have to delete tsconfig.tsbuildinfo file, because it acts as a cache file
+      // and it will not regenerate the input file.
+      // if we don't remove it and run rollup, the first run will be successful
+      // but any subsequent ones will fail because tsconfig.tsbuildinfo file exists
+      // and thinks that dist/dts/index.d.ts also exists.
+      del({ hook: "buildEnd", targets: ["./dist/dts", "tsconfig.tsbuildinfo"] })
     ],
   },
 ]
