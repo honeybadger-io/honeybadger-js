15295955697b2800189d6920ad8d47ca8de05209
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2025-03-18 08:41:50 +0200

    chore: reduce number of workers

--- packages/js/test/e2e/playwright.config.ts
+++ packages/js/test/e2e/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: 8,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [process.env.CI ? 'github' : 'list'],
