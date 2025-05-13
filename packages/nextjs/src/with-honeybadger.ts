db8c26a793cc43321fc8900c4539f3b489cd4181
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2025-05-13 20:19:57 +0300

    chore: wip

--- packages/nextjs/src/with-honeybadger.ts
+++ packages/nextjs/src/with-honeybadger.ts
@@ -12,9 +12,7 @@ function configure() {
       apiKey: process.env.NEXT_PUBLIC_HONEYBADGER_API_KEY,
       environment: process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.VERCEL_ENV || process.env.NODE_ENV,
       revision: process.env.NEXT_PUBLIC_HONEYBADGER_REVISION,
-      projectRoot: 'webpack://_N_E/./',
-      debug: true,
-      reportData: true,
+      projectRoot: 'webpack://_N_E/./'
     })
     .beforeNotify((notice) => {
       notice?.backtrace.forEach((line) => {
